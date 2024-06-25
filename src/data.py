288676dc76b11dbac2286c17898202844e32cea6
Tanay Mehta <heyytanay@gmail.com>
2024-06-25 10:27:30 +0000

    fix: data.py bugs

--- src/data.py
+++ src/data.py
@@ -1,27 +1,29 @@
 import json
+import click
 from functools import partial
 from datasets import load_dataset
-
-def create_conversation(sample, system_prompt):
-    return {
-        "messages": [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": sample["instruction"]},
-            {"role": "assistant", "content": sample["output"]}
-        ]
-    }
 
 @click.command()
 @click.option('--dataset', default='iamtarun/python_code_instructions_18k_alpaca', help='Huggingface Dataset identifier')
 @click.option('--prompt', default='prompts/python_prompt.json', help='JSON file containing the system prompt.')
 @click.option('--train_split', default=0.9, help='Ratio of samples for training set')
 @click.option('--savefile_suffix', default='alpaca_data', help='Prefix of the saved data file')
-def run(dataset, prompt, train_split, savefile_suffix):
+@click.option('--names', default=['question', 'expected_answer'], help='Instruction and Output column names in the dataset')
+def run(dataset, prompt, train_split, savefile_suffix, names):
     raw_ds = load_dataset(dataset).shuffle()
 
     # Read the prompt from the prompts file
     with open(prompt, 'r') as fl:
         SYSTEM_MSG = json.load(fl)['prompt']
+
+    def create_conversation(sample, system_prompt):
+        return {
+            "messages": [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": names[0]},
+                {"role": "assistant", "content": names[1]}
+            ]
+        }
 
     create_conversation = partial(create_conversation, system_prompt=SYSTEM_MSG)
 
@@ -37,7 +39,10 @@ def run(dataset, prompt, train_split, savefile_suffix):
     print(f"[INFO] Saving {len(train_ds)} training samples and {len(valid_ds)} validation samples.")
 
     # Save as JSON files
-    train_ds.to_json(f"data/train_{alpaca_data}.jsonl", orient='records')
-    valid_ds.to_json(f"data/valid_{alpaca_data}.jsonl", orient='records')
+    train_ds.to_json(f"data/train_{savefile_suffix}.jsonl", orient='records')
+    valid_ds.to_json(f"data/valid_{savefile_suffix}.jsonl", orient='records')
 
     print(f"[INFO] Saved successfully to 'data/'")
+
+if __name__ == "__main__":
+    run()
